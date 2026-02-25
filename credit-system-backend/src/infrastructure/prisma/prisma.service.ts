22ee25690aec8167d2238f1cff0ee37d770b78de
xdecker <xavier_deckza@hotmail.com>
2026-02-24 23:36:34 -0500

    fix: adjust configuration of prisma

--- credit-system-backend/src/infrastructure/prisma/prisma.service.ts
+++ credit-system-backend/src/infrastructure/prisma/prisma.service.ts
@@ -1,11 +1,21 @@
+import 'dotenv/config';
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from 'generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL!,
+    });
+
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
